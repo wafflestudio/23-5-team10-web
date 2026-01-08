3fff3b871e091463f3fd76cfbda6cf9216df886a
kimnamheeee <nami200284@snu.ac.kr>
2026-01-08 23:56:18 +0900

    ✨ feat(components): add action, type to navItems

--- src/widgets/navigation-sidebar/model/navItems.ts
+++ src/widgets/navigation-sidebar/model/navItems.ts
@@ -3,10 +3,11 @@ import { Compass, House, Plus, Search, UserCircle } from 'lucide-react'
 import type { ComponentType } from 'react'
 
 type NavigationSidebarItem = {
+  type: 'link' | 'button'
   label: string
   icon: ComponentType<{ className?: string }>
   to?: FileRouteTypes['to']
-  getParams?: (pathname: string) => Record<string, string | number> | undefined
+  action?: 'search' | 'create'
 }
 
 export const NAV_ITEMS: NavigationSidebarItem[] = [
@@ -14,23 +15,30 @@ export const NAV_ITEMS: NavigationSidebarItem[] = [
     label: '홈',
     icon: House,
     to: '/',
+    type: 'link',
   },
   {
     label: '검색',
     icon: Search,
+    type: 'button',
+    action: 'search',
   },
   {
     label: '탐색 탭',
     icon: Compass,
     to: '/explore',
+    type: 'link',
   },
   {
     label: '만들기',
     icon: Plus,
+    type: 'button',
+    action: 'create',
   },
   {
     label: '프로필',
     icon: UserCircle,
     to: '/me',
+    type: 'link',
   },
 ]
