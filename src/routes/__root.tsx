53ae8738bd078eaa19975297a2fdb5300f8a795b
kimnamheeee <nami200284@snu.ac.kr>
2026-01-08 17:19:17 +0900

    ✨ feat(route): configure provider

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,14 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import { Providers } from '@/app/providers/Providers'
 
 export const Route = createRootRoute({
   component: () => (
-    <>
-      <Outlet />
+    <div>
+      <Providers>
+        <Outlet />
+      </Providers>
       {import.meta.env.DEV ? <TanStackRouterDevtools /> : null}
-    </>
+    </div>
   ),
 })
