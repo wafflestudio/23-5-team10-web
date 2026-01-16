33bd61c8708fa04e0b9ecd395d999d062aac7422
kimnamheeee <nami200284@snu.ac.kr>
2026-01-16 14:20:16 +0900

    ✨ feat(msw): add mock users

--- src/mocks/db/user.db.ts
+++ src/mocks/db/user.db.ts
@@ -1,1 +1,25 @@
-export const mockPosts = []
+export interface MockUser {
+  userId: number
+  nickname: string
+  profileImageUrl: string
+}
+
+export const users: MockUser[] = [
+  { userId: 1, nickname: 'me', profileImageUrl: 'https://picsum.photos/200' },
+  {
+    userId: 2,
+    nickname: 'alice',
+    profileImageUrl: 'https://picsum.photos/200',
+  },
+  { userId: 3, nickname: 'bob', profileImageUrl: 'https://picsum.photos/200' },
+  {
+    userId: 4,
+    nickname: 'charlie',
+    profileImageUrl: 'https://picsum.photos/200',
+  },
+  {
+    userId: 5,
+    nickname: 'celebrity',
+    profileImageUrl: 'https://picsum.photos/200',
+  },
+]
