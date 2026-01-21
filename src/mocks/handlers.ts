e15b7c6b3f6b00e708b137e5f7a42ab73c5e5cad
kimnamheeee <nami200284@snu.ac.kr>
2026-01-21 20:08:19 +0900

    ✨ feat(api): add handler / api function

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,4 +1,11 @@
 import { postHandlers } from './handlers/post'
 import { commentHandlers } from './handlers/comment'
+import { feedHandlers } from './handlers/feed'
+import { testHandlers } from './handlers/test'
 
-export const handlers = [...postHandlers, ...commentHandlers]
+export const handlers = [
+  ...postHandlers,
+  ...commentHandlers,
+  ...feedHandlers,
+  ...testHandlers,
+]
